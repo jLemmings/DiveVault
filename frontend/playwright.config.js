dc19c755a2e4aa19a66e84428cf0a5554c89cb19
jLemmings <jsohua@hemmings.ch>
2026-04-07 19:18:53 +0200

    update playwright output

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -5,7 +5,9 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
-  reporter: process.env.CI ? [["github"], ["html", { open: "never" }]] : [["list"], ["html", { open: "never" }]],
+  reporter: process.env.CI
+    ? [["list"], ["github"], ["html", { open: "never" }]]
+    : [["list"], ["html", { open: "never" }]],
   use: {
     baseURL: "http://127.0.0.1:4173",
     trace: "on-first-retry",
