1a4339a96252a12dabaab4deccd7532f21db1003
jLemmings <jsohua@hemmings.ch>
2026-05-24 09:28:04 +0200

    fix frontend tests

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI
     ? [["list"], ["github"], ["html", { open: "never" }]]
     : [["list"], ["html", { open: "never" }]],
