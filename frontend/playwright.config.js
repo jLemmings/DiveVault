e73d65501ca93967041a0d94c99fd89d9244f707
jLemmings <jsohua@hemmings.ch>
2026-05-24 09:40:00 +0200

    fix frontend tests

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -2,10 +2,10 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
-  fullyParallel: true,
+  fullyParallel: !process.env.CI,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   reporter: process.env.CI
     ? [["list"], ["github"], ["html", { open: "never" }]]
     : [["list"], ["html", { open: "never" }]],
